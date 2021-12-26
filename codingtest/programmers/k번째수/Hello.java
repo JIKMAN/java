89b46a975e7177541aa07550ca410e2f186d8f41
Jungik <wjddlr0303@gmail.com>
2021-12-26 21:50:46 +0900

    Add k번째수

---
+++ codingtest/programmers/k번째수/Hello.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Hello {
+    public static void main(String[] args) {
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2,5,3},{4,4,1},{1,7,3}};
+
+        int[] answer = new int[commands.length] ;
+        
+        for (int i = 0; i < commands.length; i++) {
+            int[] temp = new int[commands[i][1]-(commands[i][0]-1)];
+            for (int j = 0; j < temp.length; j++) {
+                temp[j] = array[j+(commands[i][0]-1)];
+            }
+            Arrays.sort(temp);
+            answer[i]= temp[commands[i][2]-1];
+        }
+        System.out.println(Arrays.toString(answer));
+    }
+}
