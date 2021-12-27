7f444acec3ac2f308378996aca3068a52b005efa
Jungik <wjddlr0303@gmail.com>
2021-12-27 21:12:48 +0900

    Change name hello to solution

--- codingtest/programmers/k번째수/Solution.java
+++ codingtest/programmers/k번째수/Solution.java
@@ -5,15 +5,16 @@ public class Solution {
         int[] array = {1,5,2,6,3,7,4};
         int[][] commands = {{2,5,3}, {4,4,1}, {1,7,3}};
 
-        int[] answer = new int[commands.length];
-    
-        for(int i=0; i<commands.length; i++){
-            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+        int[] answer = new int[commands.length] ;
+        
+        for (int i = 0; i < commands.length; i++) {
+            int[] temp = new int[commands[i][1]-(commands[i][0]-1)];
+            for (int j = 0; j < temp.length; j++) {
+                temp[j] = array[j+(commands[i][0]-1)];
+            }
             Arrays.sort(temp);
-            answer[i] = temp[commands[i][2]-1];
-       }
-
-       System.out.println(Arrays.toString(answer));
-
+            answer[i]= temp[commands[i][2]-1];
+        }
+        System.out.println(Arrays.toString(answer));
     }
 }
